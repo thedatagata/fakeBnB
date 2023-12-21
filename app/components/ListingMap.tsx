413479c42a62c74b3ec51d1fa4a28dc44247c431
Yale Newman <data_gata@wtmbpyalenewman.lan>
2023-12-21 14:00:35 -0500

    chatgpt ftw

--- app/components/ListingMap.tsx
+++ app/components/ListingMap.tsx
@@ -1,57 +1,88 @@
-"use client";
-import React from 'react'
-import ReactMapGL, {Marker, Popup} from 'react-map-gl'
-import {useState} from 'react'
-import getCenter from 'geolib/es/getCenter'
+'use client'
+import React, { useState, useEffect, useRef } from 'react';
+import Map, { Marker } from 'react-map-gl';
+import getCenter from 'geolib/es/getCenter';
 import { FaMapMarkerAlt } from "react-icons/fa";
+import { WebMercatorViewport } from '@math.gl/web-mercator';
 
-interface ListingMapProps {
-  listings: any
-};
-
-
-
-const ListingMap: React.FC<ListingMapProps> = ({ listings }) => {
-  const listingCoords = listings.map((listing: any) => ({
-    latitude: listing.latitude,
-    longitude: listing.longitude
-  }));
-  console.log(listingCoords);
-  const mapCenter = getCenter(listingCoords); 
-  console.log(mapCenter);
-  const [viewState, setViewState] = useState({
-    latitude: mapCenter.latitude,
-    longitude: mapCenter.longitude,
-    zoom: 10,
-  })
-
-
-  return (
-    <ReactMapGL
-     mapboxAccessToken={process.env.mapbox_key}
-     style={{width: "100%", height: "100%"}}
-     mapStyle={process.env.style_url}
-     initialViewState={{
-      ...viewState
-     }}
-     onMove={evt => setViewState(evt.viewState)}
-    >
-     {listings.map((listing: any)=>(
-      <div key={listing.id}>
-        <Marker
-         key={listing.id}
-         longitude={listing.longitude} 
-         latitude={listing.latitude}
-        >
-          <p className="cursor-pointer text-2xl animate-bounce">
-            <FaMapMarkerAlt />
-          </p>
-        </Marker>
-      </div>
-     ))}
-    </ReactMapGL>
-
-  )
+interface Listing {
+  id: string;
+  title: string;
+  latitude: number;
+  longitude: number;
 }
 
-export default ListingMap
+interface ListingMapProps {
+  listings: Listing[];
+};
+
+const ListingMap: React.FC<ListingMapProps> = ({ listings }) => {
+  console.log(listings);
+  const [viewState, setViewState] = useState({
+    latitude: 0,
+    longitude: 0,
+    zoom: 10,
+  });
+
+  const mapContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (listings.length === 0 || !mapContainerRef.current) {
+      return;
+    }
+
+    const coordinates = listings.map(listing => ({
+      latitude: listing.latitude,
+      longitude: listing.longitude
+    }));
+
+    const mapCenter = getCenter(coordinates);
+    const boundsRect = mapContainerRef.current.getBoundingClientRect();
+
+    let bounds = [
+      [Infinity, Infinity],
+      [-Infinity, -Infinity]
+    ];
+
+    coordinates.forEach(coord => {
+      bounds = [
+        [Math.min(bounds[0][0], coord.longitude), Math.min(bounds[0][1], coord.latitude)],
+        [Math.max(bounds[1][0], coord.longitude), Math.max(bounds[1][1], coord.latitude)]
+      ];
+    });
+
+    const viewport = new WebMercatorViewport({ width: boundsRect.width, height: boundsRect.height })
+      .fitBounds([bounds[0], bounds[1]], { padding: 20 });
+
+    setViewState({
+      latitude: mapCenter.latitude, // Corrected to use mapCenter
+      longitude: mapCenter.longitude,
+      zoom: viewport.zoom
+    });
+  }, [listings]);
+
+  return (
+    <div ref={mapContainerRef} className="w-full h-full">
+      <Map
+        mapboxAccessToken={process.env.mapbox_key}
+        mapStyle={process.env.style_url}
+        {...viewState}
+        onMove={evt => setViewState(evt.viewState)}
+      >
+        <div className="markers-container">
+          {listings.map((listing) => (
+              <Marker key={listing.id} longitude={listing.longitude} latitude={listing.latitude}>
+                <p className="cursor-pointer text-2xl animate-bounce">
+                  <FaMapMarkerAlt />
+                </p>
+              </Marker>
+            ))}
+        </div>
+      </Map>
+    </div>
+  );
+};
+
+export default ListingMap;
+
+
